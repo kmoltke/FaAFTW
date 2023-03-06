7b0ecc3c3c93840f8191fef0237fec3bac677c23
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2023-03-06 14:59:23 +0100

    bla

--- scripts/basket.js
+++ scripts/basket.js
@@ -5,3 +5,30 @@ function addToBasket(itemId) {
     console.log(`Hey ${itemId}`);
 }
 
+function renderBasketCard(vinyls, productId) {
+    const product = vinyls.find((x) => x.id === productId);
+
+    const basketCardContainer = document.createElement("div");
+    basketCardContainer.className
+
+    const image = document.createElement("img");
+
+
+
+    const albumElement = document.createElement("a");
+    albumElement.className = "productTitle";
+    albumElement.href = "product-description.html?id=" + element.id;
+    albumElement.innerHTML = `${element.album}`;
+
+    const artistElement = document.createElement("p");
+    artistElement.className = "productArtist";
+    artistElement.innerHTML = `${element.artist}`;
+
+    const priceElement = document.createElement("p");
+    priceElement.className = "productPrice";
+    priceElement.innerHTML = `${element.price},-`;
+}
+
+function renderBasketList() {
+
+}
