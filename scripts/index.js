0d942db19e2c2fda2ff9096129c6d51d29ecde54
Eleonora Borovic Kristensen <eleb@itu.dk>
2023-03-07 22:48:17 +0100

    fixed bug

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,16 @@
-const loginLink = document.getElementById('login-link')
-const cartLink = document.getElementById('cart-link')
-//Logout link has a default display of nones
-const logoutLink = document.getElementById('logout-link')
-logoutLink.style.display = 'none'
-
-const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+let data;
 
 document.body.onload = async () => {
-  const vinyls = await loadProductDataAsync()
-  DisplayFeatured(vinyls)
-  renderPage(vinyls)
-}
+  const vinyls = await loadProductDataAsync();
+  DisplayFeatured(vinyls);
+  renderPage(vinyls);
+  data = vinyls;
+};
 
 function scrollIntoBrowse() {
-  const element = document.getElementById('browseContainer')
-  element.scrollIntoView()
+  const element = document.getElementById("browseContainer");
+  element.scrollIntoView();
 }
 function scrollIntoHome() {
-  window.scrollTo(0, 0)
-}
-
-if (userInfo) {
-  console.log(userInfo)
-  logoutLink.style.display = 'block'
-  loginLink.style.display = 'none'
-
-  const splitName = userInfo.user.split(' ')
-
-  cartLink.textContent = `${splitName[0]}´s Cart`
-
-  logoutLink.addEventListener('click', () => {
-    localStorage.removeItem('userInfo')
-  })
+  window.scrollTo(0, 0);
 }
--- scripts/login-form.js
+++ scripts/login-form.js
@@ -1,38 +1,38 @@
 //DOM selectors
-const loginForm = document.getElementById('loginForm')
-const nameInput = document.getElementById('fullNameInput')
-const emailInput = document.getElementById('emailInput')
-const passwordInput = document.getElementById('passwordInput')
-const rememberMeCheckbox = document.getElementById('rememberMeCheckbox')
+const loginForm = document.getElementById("loginForm");
+const nameInput = document.getElementById("fullNameInput");
+const emailInput = document.getElementById("emailInput");
+const passwordInput = document.getElementById("passwordInput");
+const rememberMeCheckbox = document.getElementById("rememberMeCheckbox");
 
 //loginInformation initial state
 const loginInformation = {
-  user: '',
-  email: '',
-  password: '',
-  remember: 'off',
-}
+  user: "",
+  email: "",
+  password: "",
+  remember: "off",
+};
 
 //Event handlers
-nameInput.addEventListener('change', (e) => {
-  loginInformation.user = e.target.value
-})
-emailInput.addEventListener('change', (e) => {
-  loginInformation.email = e.target.value
-})
-passwordInput.addEventListener('change', (e) => {
-  loginInformation.password = e.target.value
-})
-rememberMeCheckbox.addEventListener('change', (e) => {
-  loginInformation.remember = e.target.value
-})
+nameInput.addEventListener("change", (e) => {
+  loginInformation.user = e.target.value;
+});
+emailInput.addEventListener("change", (e) => {
+  loginInformation.email = e.target.value;
+});
+passwordInput.addEventListener("change", (e) => {
+  loginInformation.password = e.target.value;
+});
+rememberMeCheckbox.addEventListener("change", (e) => {
+  loginInformation.remember = e.target.value;
+});
 
 //Form submission
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  localStorage.setItem('userInfo', JSON.stringify(loginInformation))
-  alert('You have logged in successfully, you will be redirected shortly..')
-  window.location.replace('/index.html')
-  loginForm.reset()
-})
+  localStorage.setItem("userInfo", JSON.stringify(loginInformation));
+  alert("You have logged in successfully, you will be redirected shortly..");
+  window.location.replace("../pages/index.html");
+  loginForm.reset();
+});
