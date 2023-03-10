f0a1fcc4157f3c86197cd92f874270fb09a06184
spkr <spkr@itu.dk>
2023-03-10 15:53:41 +0100

    Added a little JSDoc

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 let data;
 
+/**
+ * Sets up the page, loading products data, displaring vinyls
+ */
 document.body.onload = async () => {
   const vinyls = await loadProductDataAsync();
   DisplayFeatured(vinyls);
@@ -7,10 +10,17 @@ document.body.onload = async () => {
   data = vinyls;
 };
 
+/**
+ * Scrolls the window down to "browsecontainer"
+ */
 function scrollIntoBrowse() {
   const element = document.getElementById("browseContainer");
   element.scrollIntoView();
 }
+
+/**
+ * Scrools the window to the top of the page
+ */
 function scrollIntoHome() {
   window.scrollTo(0, 0);
 }
