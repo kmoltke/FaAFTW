c6199a5e2147bc3227a11b74ab868692626c0b28
mlup <mlup@itu.dk>
2023-04-12 15:48:26 +0200

    pair programming: corrected typo

--- api/src/users/users.route.ts
+++ api/src/users/users.route.ts
@@ -1,6 +1,6 @@
-import express from "express"
-import { createUser } from "./users.controler"
+import express from 'express'
+import { createUser } from './users.controler'
 
-export const userRouter = express.Router()
+export const usersRouter = express.Router()
 
-userRouter.post("/users", createUser)
+usersRouter.post('/users', createUser)
