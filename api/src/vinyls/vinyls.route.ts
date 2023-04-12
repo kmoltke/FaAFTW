b0b7688302d331f3a4c6c1f81e9d2928c1255fdf
mlup <mlup@itu.dk>
2023-04-12 12:42:21 +0200

    solved merge conflicts

--- api/src/vinyls/vinyls.route.ts
+++ api/src/vinyls/vinyls.route.ts
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-import express from "express"
-import { getAllVinyls, getVinyl } from "./vinyls.controler"
-
-export const vinylsRouter = express.Router()
-
-vinylsRouter.get("/products", getAllVinyls)
-vinylsRouter.get("/products/:id", getVinyl)
-=======
 import express from 'express'
-import { addVinyl, getAllVinyls, getVinylById } from './vinyls.controler'
+import { getAllVinyls, getVinyl } from './vinyls.controler'
 
 export const vinylsRouter = express.Router()
 
 vinylsRouter.get('/products', getAllVinyls)
-vinylsRouter.get('/products/:id', getVinylById)
-vinylsRouter.post('/products', addVinyl)
->>>>>>> cc0ff2b0f509bd8803cf52b5c2d523e8c562de51
+vinylsRouter.get('/products/:id', getVinyl)
