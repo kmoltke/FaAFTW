74dfab18910a527d5a8e7d9f3187220fbc868816
mlup <mlup@itu.dk>
2023-04-26 13:43:45 +0200

    started migration of GridCard

---
+++ client/src/components/GridCard/GridCard.tsx
@@ -0,0 +1,30 @@
+import '../styles/index.css'
+import '../styles/template.css'
+
+function GridCard() {
+  return (
+    <>
+      <div className="productCard">
+        <a href="product-description.html?id=2">
+          <img className="image" src="../images/id2-1.jpg"></img>
+        </a>
+        <div className="productDetails">
+          <a className="productTitle" href="product-description.html?id=2">
+            IV
+          </a>
+          <p className="productArtist">Led Zeppelin</p>
+        </div>
+        <p className="productPrice">177,-</p>
+        <button
+          className="cardButton btn btn-primary"
+          id="liveAlertBtn"
+          // onclick="addToBasket(2);"
+        >
+          Add to Cart
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default GridCard
