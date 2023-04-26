a7b61e78b39e3ba35f293dc5419931dff08fac5d
mlup <mlup@itu.dk>
2023-04-26 13:43:45 +0200

    started migration of GridCard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
-import "./styles/template.css"
-import "./styles/navbar.css"
-import "./styles/basket.css"
+import './styles/template.css'
+import './styles/navbar.css'
+import './styles/basket.css'
 
-import Router from "./router/Router"
+import Router from './router/Router'
+import GridCard from './components/GridCard/GridCard'
 
 function App() {
   return (
-    <Router />
-  
+    <>
+      <Router />
+      <GridCard />
+    </>
   )
 }
 
