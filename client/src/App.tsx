44c32fcfd2f7baba5d870676992751187cb96826
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2023-04-27 22:22:40 +0200

    Add Summary

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import "./styles/basket.css"
 import Router from "./router/Router"
 import GridCard from "./components/GridCard/GridCard"
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <>
--- client/src/pages/CartPage/CartPage.tsx
+++ client/src/pages/CartPage/CartPage.tsx
@@ -1,5 +1,67 @@
+import {useEffect, useState} from "react";
+import {Card, Col, Container, Row} from "react-bootstrap";
+import BasketSummary from "../../components/BasketSummary/BasketSummary";
+import { Basket } from "../../../../api/src/baskets/baskets.model";
+
 function CartPage() {
-  return <>cart</>
+    const emptyBasket: Basket = {
+        id: 0,
+        BasketId: 0,
+        products: [],
+        total: 0
+    };
+    const [basket, setProducts] = useState(emptyBasket)
+
+    useEffect(() => {
+        const api = async () => {
+            const data = await fetch("http://localhost:5000/users/1680705564259/basket")
+            const jsonData = await data.json()
+            setProducts(jsonData)
+        }
+
+        api()
+    }, [])
+
+
+    return (
+        <Container>
+            <Card>
+                <Row>
+                    <Col sm={8}>
+                        <h3>Products</h3>
+                        <hr/>
+                    </Col>
+                    <Col sm={4}>
+                        <h3>Summary</h3>
+                        <hr/>
+                        <BasketSummary total={basket.total}></BasketSummary>
+                    </Col>
+                </Row>
+            </Card>
+        </Container>
+    )
+
+    // return (
+    //     <Container>
+    //         <Row>
+    //             <Col sm={8}>
+    //                 <Card>
+    //                     <Card.Body>
+    //                         Hello
+    //                     </Card.Body>
+    //                 </Card>
+    //             </Col>
+    //             <Col sm={4}>
+    //                 <Card>
+    //                     <Card.Body>
+    //                         Summary
+    //                     </Card.Body>
+    //                 </Card>
+    //             </Col>
+    //         </Row>
+    //     </Container>
+    // )
 }
+
 
 export default CartPage
