6e99cbdefee4b3496ee7fb482e2dc1cb9b02000d
spkr <spkr@itu.dk>
2023-04-27 17:33:39 +0200

    import navbar

--- client/src/pages/ProductDescriptionPage/ProductDescriptionPage.tsx
+++ client/src/pages/ProductDescriptionPage/ProductDescriptionPage.tsx
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import Navbar from "../../components/Navbar/Navbar"
 
 function ProductDescriptionPage() {
   const { id } = useParams()
