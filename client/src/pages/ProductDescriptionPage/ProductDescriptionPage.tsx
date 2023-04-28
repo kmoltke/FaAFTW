804ddae2c4738db214bf4ad76271a063c1eb6f42
spkr <spkr@itu.dk>
2023-04-28 15:08:05 +0200

    Latest work on productdesc

--- client/src/pages/ProductDescriptionPage/ProductDescriptionPage.tsx
+++ client/src/pages/ProductDescriptionPage/ProductDescriptionPage.tsx
@@ -1,14 +1,53 @@
 import { useParams } from "react-router-dom"
-import { NavLink } from "react-router-dom"
-import Navbar from "../../components/Navbar/Navbar"
+import "./ProductDescriptionPage.css"
+import vinyls from '../../../../api/data/vinyls.json';
 
-function ProductDescriptionPage() {
-  const { id } = useParams()
-  return (
-    <div className="container">
-      <h1>Products Details Page - {id}</h1>
-    </div>
-  )
+const addToCart = () => {
+  
 }
 
+type Vinyl = {
+  id: number
+  artist: string
+  album: string
+  decade: string
+  year: number
+  genre: string
+  price: number
+  type: string
+  label: string
+  image: string
+  description: string
+}
+
+
+function ProductDescriptionPage(): JSX.Element {
+  const { id } = useParams() 
+  const path = "/products/" + id
+  const vinyl = vinyls.find((vinyl) => vinyl.id === id) as Vinyl 
+
+return (
+<div className="container">
+    <h1> Product description - {id} </h1>
+    <section>
+      <div className="productOuter" id="productOuter">
+        <div className="productImage"> {vinyl.image}</div>
+        <div className="productOverview" id="productOverview"></div>
+        <h1 className="productTitle"> {vinyl.album}</h1>
+        <h3 className="productArtist"> {vinyl.artist}</h3>
+        <p className="productPrice"> {vinyl.price}</p>
+        <button onClick={addToCart}> Add to Cart </button>
+      </div>
+      <div className="productDetails" id="productDetails"></div>
+      <p> Year: {vinyl.year} </p>
+      <p> Genre: {vinyl.genre} </p>
+      <p> Type: {vinyl.type} </p>
+      <p> Label: {vinyl.label} </p>
+    </section>
+  </div>
+)
+};
+
 export default ProductDescriptionPage
+
+
