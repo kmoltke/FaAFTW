69eb79e01b45a3b1bd28eea5ab96dafd60755f3c
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2023-05-03 22:41:44 +0200

    minor cleanup

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -10,10 +10,8 @@ function Navbar() {
         throw new Error("User is undefined")
     const {user, updateUser} = ctx
     const name = user.fname
-    console.log("Navbaruser", user)
 
     useEffect(() => {
-        console.log('Navbar user effect:', user);
     }, [user]);
 
     const [responsive, setResponsive] = useState("")
@@ -59,8 +57,6 @@ function Navbar() {
                         <NavLink to={"/cart"} className={styles.navItem}>
                             {isLoggedIn() ? (
                                 <>
-                                    {console.log("NavLink user", user)}
-                                    {console.log("Navlink fname:", name)}
                                     <span style={{color: "#f23737"}}>{user?.fname}</span>
                                     {"'s Cart"}
                                 </>
