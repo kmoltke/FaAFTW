1de35395b9e7132d87429cf8aa8d9811b0d68b4c
spkr <spkr@itu.dk>
2023-05-05 08:53:39 +0200

    dynamic carousel

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "./Carousel.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Vinyl } from "../../../../api/src/vinyls/vinyls.model";
 
 function ImageCarousel() {
+  const [products, setProducts] = useState<any[]>();
+
+  useEffect(() => {
+    fetch("http://localhost:5000/products/")
+      .then((data) => data.json())
+      .then((parsedData) => setProducts(parsedData));
+  }, []);
+
+  console.log("featured products from carousel", products);
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <h1> Featured new releases </h1>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row></Row>
+        <Carousel>
+          {products?.map((product) => {
+            if (product?.featured) {
+              return (
+                <Carousel.Item>
+                  <Link to="/products/">
+                    <img className="carousel-img" src={product?.imageSrc} />
+                  </Link>
+                  <Carousel.Caption>
+                    <p className="carousel h3"> {product?.album} </p>
+                    <p className="carousel h3"> {product?.artist} </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            }
+          })}
+        </Carousel>
+      </Container>
+    </>
+  );
+}
+
+export default ImageCarousel;
+
+/*
+
   return (
     <>
       <Container>
@@ -63,4 +111,4 @@ function ImageCarousel() {
   );
 }
 
-export default ImageCarousel;
+*/
